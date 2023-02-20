494be4cc438978e666104b33f154eeebcf6cd37f
Damian Grembocki <damian.grembocki@o2.pl>
2023-02-20 13:34:27 +0100

    Commit

--- RandomNumberGenerator_fromSetRange/Program.cs
+++ RandomNumberGenerator_fromSetRange/Program.cs
@@ -1,6 +1,6 @@
 ﻿try
 {
-    int min, max, howMuch;
+    int min, max, howMany;
     Random randomNumber = new Random();
 
     Console.WriteLine("Hey, I will pick and print for you choosen number of random numbers from choosen range.");
@@ -9,8 +9,8 @@
     Console.WriteLine("Enter maximum range: ");
     max = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("How many random numbers you want me to print: ");
-    howMuch = Convert.ToInt32(Console.ReadLine());
-    if (howMuch <= 0) {
+    howMany = Convert.ToInt32(Console.ReadLine());
+    if (howMany <= 0) {
         throw new Exception("Wrong amount of random numbers!");
     }
     if (max <= min)
@@ -18,7 +18,7 @@
         throw new Exception("Wrong numbers range! Max cannot be smaller or equal then max!");
     }
 
-    for (int i = 1; i <= howMuch; i++) {
+    for (int i = 1; i <= howMany; i++) {
         Console.WriteLine("Number {0} random is: " + randomNumber.Next(min, max+1), i);
     }
     
